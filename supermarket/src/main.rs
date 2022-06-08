c8427a8a5757a3c88d7f7f88a5dcb62304b57688
Gallottino <d.gallotti@outlook.it>
2022-06-08 10:46:54 +0200

    Added market logic

--- supermarket/src/main.rs
+++ supermarket/src/main.rs
@@ -1,7 +1,17 @@
+/* ##############################################
+ *              NFT Chain Shop
+ * A simple digital market with sellable object.
+ * ##############################################
+ */
+
 mod user;
 
+use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::io::*;
+use std::str;
+use user::*;
 
 pub fn flush() {
     std::process::Command::new("clear").status().unwrap();
@@ -9,7 +19,19 @@ pub fn flush() {
 
 fn print_action() {
     flush();
-    println!("■■■■■■■■■■■■■ Welcome to *NFT Chain* shop!■■■■■■■■■■■■■■■■■■■■■");
+    println!("
+ _   _ ______ _______    _____          _    _ _____ _   _    _____ _    _  ____  _____  
+| \\ | |  ____|__   __|  / ____|   /\\   | |  | |_   _| \\ | |  / ____| |  | |/ __ \\|  __ \\ 
+|  \\| | |__     | |    | |       /  \\  | |__| | | | |  \\| | | (___ | |__| | |  | | |__) |
+| . ` |  __|    | |    | |      / /\\ \\ |  __  | | | | . ` |  \\___ \\|  __  | |  | |  ___/ 
+| |\\  | |       | |    | |____ / ____ \\| |  | |_| |_| |\\  |  ____) | |  | | |__| | |     
+|_| \\_|_|       |_|     \\_____/_/    \\_\\_|  |_|_____|_| \\_| |_____/|_|  |_|\\____/|_|
+            ");
+}
+
+fn manage_action() -> usize {
+
+    return 0;
 }
 
 // The program takes as input the filename
@@ -17,16 +39,75 @@ fn print_action() {
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let filename = &args[1]; // It must contains a filename
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    let catalog = json::parse(&contents.to_string()).unwrap();
+    let mut warehouse: HashMap<String,Sellable>;
+    warehouse = load_json(filename.to_string());
 
-    print_action();
+    let mut budget: f64 = 2.50;
 
-    println!("{}", catalog);
+    let mut action_mesage: String = String::from("Welcome to our shop. Take a look around!\n");
     loop {
 
+        print_action();
+        println!("{}", action_mesage);
+        println!("Your current budget: {:.2}€\n", budget);
+
+        for object in warehouse.values() {
+            println!("{}",object);
+        }
+ 
+        let mut buffer = String::new();
+
+        print!("Tip the item that you want to buy: ");
+        std::io::stdout().flush().unwrap();
+        let stdin = std::io::stdin(); // We get `Stdin` here.
+        let err = stdin.read_line(&mut buffer).err();
+        
+        if !err.is_none() {
+            println!("{:?}", err);
+            return;
+        }
+        buffer = buffer.replace("\n", "");
+        
+        let key = buffer.to_lowercase();
+        if warehouse.contains_key(&key)  {
+            
+            let sellable = &warehouse[&key];
+
+            if budget >= sellable.price {
+                action_mesage = String::from("I love your money ＄＄＄\nWould you like to buy something else?");
+                budget -= sellable.price;
+                warehouse.remove(&key);
+            }
+            else {
+                action_mesage = String::from("Oh! It seems that you have not enough money....");
+            }
+        }
+        else {
+            action_mesage = String::from("We don't sell that kind of things, man!");
+        }
     }
+}
+
+pub fn load_json (filename: String) -> HashMap<String, Sellable> {
+
+    let mut new_warehouse: HashMap<String,Sellable>  = HashMap::new();
+    let contents = fs::read_to_string(filename)
+                        .expect("Something went wrong reading the file");
+
+    let catalog = json::parse(&contents.to_string()).unwrap();
+
+    for i in 0..catalog["items"].len() {
+        let name = catalog["items"][i]["name"].to_string();
+        let price = catalog["items"][i]["price"].to_string();
+        let description = catalog["items"][i]["description"].to_string();
+
+        new_warehouse.insert(
+            name.to_lowercase(),
+            create_sellable(price.parse::<f64>().unwrap(), name, description)
+        );
+    }
+
+    return new_warehouse;
 }
--- supermarket/src/user.rs
+++ supermarket/src/user.rs
@@ -4,18 +4,18 @@ use std::fmt;
 #[derive(Debug)]
 
 pub struct Sellable {
-    price: u128,
-    name: String,
-    description: String,
+    pub price: f64,
+    pub name: String,
+    pub description: String,
 }
 
 impl fmt::Display for Sellable {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
-        write!(f, "Name: {},\n Price:{},\n Description: {} items",self.name, self.price, self.description)
+        write!(f, "█ Name: {},\n█ Price: {:.2}€,\n█ Description: {} \n",self.name, self.price, self.description)
     }
 }
 
-pub fn create_sellable( self_price: u128, selfname: String, self_description: String) -> Sellable {
+pub fn create_sellable( self_price: f64, selfname: String, self_description: String) -> Sellable {
     return Sellable {
         name: selfname,
         price: self_price,
@@ -32,8 +32,8 @@ pub struct User {
 impl User {
     pub fn add_sellable(&mut self, object: Sellable) {
         self.owned.insert(
-            object.name.clone(), 
-            create_sellable(object.price, object.name.clone(), object.description)
+            object.name.to_string(), 
+            create_sellable(object.price, object.name.to_string(), object.description)
         );
     }
 }
